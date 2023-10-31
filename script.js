84bb69e688b4110d822213408945b9fc1fa78b5e
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-10-31 13:45:22 -0400

    add script

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+p.textContent = "This is a paragraph with content on my personal website.";
+
+document.querySelector("body").appendChild(p);
